37b2c8994eabb9d662560284cdfe3f0d9d3a8b84
Kevin Eye <kevineye@gmail.com>
2013-08-20 07:04:20 -0400

    fixed IE errors

--- appendAround.js
+++ appendAround.js
@@ -1,11 +1,13 @@
-/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL 
-how-to:
-	1. Insert potential element containers throughout the DOM
-	2. give each container a data-set attribute with a value that matches all other containers' values
-	3. Place your appendAround content in one of the potential containers
-*/
+/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL
+ how-to:
+ 1. Insert potential element containers throughout the DOM
+ 2. give each container a data-set attribute with a value that matches all other containers' values
+ 3. Place your appendAround content in one of the potential containers
+ */
 (function( $ ){
     $.fn.appendAround = function(){
+        if(!window.getComputedStyle) return;
+
         return this.each(function(){
 
             var $self = $( this ),
